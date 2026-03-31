6437481fa1be49df93f15186f5bfca0877ec7582
Lexi Rose <north@tinymagnet.com>
2026-03-31 14:43:47 -0500

    demo effect tweak

--- Assets/Scripts/Crowd Control/Effects/CreateSpheres.cs
+++ Assets/Scripts/Crowd Control/Effects/CreateSpheres.cs
@@ -8,14 +8,37 @@ public class CreateSpheres : UnityEffectBase
 {
     public GameObject BallPrefab;
 
-    public Transform SpawnLocation;
+    private Camera m_camera;
+
+    private CameraFollowBehavior m_cameraFollow;
+
+    private static readonly Vector3 SPAWN_OFFSET = new(0f, 5f, 0f);
+
+    protected Transform SpawnLocation
+    {
+        get
+        {
+            if (!m_cameraFollow) return gameObject.transform;
+            return m_cameraFollow.target.transform;
+        }
+    }
 
     public CreateSpheres([NotNull] CrowdControl.Client.WebSocket.CrowdControl crowdControl, [NotNull] ClientSocket client) : base(crowdControl, client) { }
 
+    protected override void Awake()
+    {
+        m_camera = Camera.main;
+        m_cameraFollow = m_camera.GetComponent<CameraFollowBehavior>();
+        base.Awake();
+    }
+
     public override EffectStatus StartEffect(EffectRequest request)
     {
-        if (!BallPrefab || !SpawnLocation) return EffectStatus.FailPermanent;
-        Instantiate(BallPrefab, SpawnLocation.position, SpawnLocation.rotation);
+        if ((!m_camera) || (!BallPrefab) || (!SpawnLocation)) return EffectStatus.FailPermanent;
+
+        //add a meter in the camera look direction and spawn the ball there
+        Vector3 position = SpawnLocation.position + SPAWN_OFFSET;
+        Instantiate(BallPrefab, position, Quaternion.identity);
         return EffectStatus.Success;
     }
 }
