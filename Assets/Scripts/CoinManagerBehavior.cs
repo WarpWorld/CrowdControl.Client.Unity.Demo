5faa38a413fdb3a54b356037ffab2f40cff68a9e
Lexi Rose <north@tinymagnet.com>
2026-09-25 15:41:35 -0500

    Emit Crowd Control coin collection event
    Hook CoinManagerBehavior up to CrowdControlBehavior so each collected coin triggers a `CoinCollected` event with the current coin count. This also removes the unused per-instance GUID state in favor of shared event payload data.

--- Assets/Scripts/CoinManagerBehavior.cs
+++ Assets/Scripts/CoinManagerBehavior.cs
@@ -1,25 +1,35 @@
-using System;
+using CrowdControl.Client.Unity;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class CoinManagerBehavior : MonoBehaviour
 {
+    private CrowdControlBehavior m_ccBehavior;
+
     private static readonly WaitForSeconds WAIT_30 = new(30f);
     public TextMeshProUGUI CoinCounter;
 
     public int CollectedCoinCount { get; private set; }
 
     private List<GameObject> m_allCoins = new();
+    private readonly ConcurrentDictionary<string, object?> m_coinCollectedEventArgs = new()
+    {
+        ["coins"] = 0
+    };
 
     private const string COIN_TAG = "Coin";
 
-    private Guid m_instanceID;
+
+    private void Awake()
+    {
+        m_ccBehavior = FindAnyObjectByType<CrowdControlBehavior>();
+    }
 
     void Start()
     {
-        m_instanceID = Guid.NewGuid();
         m_allCoins.AddRange(GameObject.FindGameObjectsWithTag(COIN_TAG));
         UpdateCounterText();
     }
@@ -37,6 +47,8 @@ public class CoinManagerBehavior : MonoBehaviour
         }
         StartCoroutine(respawn());
 
+        m_coinCollectedEventArgs["coins"] = CollectedCoinCount;
+        if (m_ccBehavior) m_ccBehavior.TriggerEvent("CoinCollected", m_coinCollectedEventArgs);
         CollectedCoinCount++;
         UpdateCounterText();
         return true;
@@ -50,7 +62,6 @@ public class CoinManagerBehavior : MonoBehaviour
 
     public void ResetCoins()
     {
-        m_instanceID = Guid.NewGuid();
         CollectedCoinCount = 0;
         UpdateCounterText();
     }
